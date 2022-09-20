5510ae00f45624e238d37906db351f1e44a78b78
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2022-09-20 19:56:22 -0400

    Fixed enderman mixin to use HostileEntity instead of LivingEntity
    Signed-off-by: Eman3600 <98138568+Eman-3600@users.noreply.github.com>

--- src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
+++ src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,9 +14,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndermanEntity.class)
-public abstract class EndermanEntityMixin extends LivingEntity implements Angerable {
+public abstract class EndermanEntityMixin extends HostileEntity implements Angerable {
 
-    protected EndermanEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+    protected EndermanEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
     }
 
