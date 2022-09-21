4941875fa616638c8866941b20c18dce6aff224d
stormdirus2 <stormdirus2@protonmail.com>
2022-09-21 15:05:57 -0500

    * Reworked SonicBoomDisabler so it actually disabled the sonic boom

--- src/main/java/net/eman3600/dndreams/mixin/SonicBoomDisabler.java
+++ src/main/java/net/eman3600/dndreams/mixin/SonicBoomDisabler.java
@@ -1,15 +1,18 @@
 package net.eman3600.dndreams.mixin;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.task.SonicBoomTask;
-import net.minecraft.entity.mob.WardenEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SonicBoomTask.class)
 public abstract class SonicBoomDisabler {
-    public boolean shouldRun(ServerWorld serverWorld, WardenEntity wardenEntity) {
-        return false;
+    @Inject(method = "shouldRun(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
+    public void dndreams$disasbleSonicBoom(ServerWorld world, LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
     }
+
 }
