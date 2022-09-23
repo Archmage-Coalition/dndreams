634ecc289009e9b12fc231a5a4643ee8eeefa35a
stormdirus2 <stormdirus2@protonmail.com>
2022-09-23 11:20:18 -0500

    * Use ModifyArg, and only on the relevant method, for better compatibility.

--- src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
+++ src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
@@ -4,19 +4,17 @@ import net.eman3600.dndreams.initializers.ModDimensions;
 import net.eman3600.dndreams.initializers.WorldComponents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -41,14 +39,8 @@ public abstract class EndermanEntityMixin extends HostileEntity implements Anger
         }
     }
 
-    @Redirect(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V"))
-    private void redirectInitGoals(GoalSelector instance, int priority, Goal goal) {
-        if (goal instanceof RevengeGoal) {
-            instance.add(priority, new RevengeGoal(this, new Class[] {
-                    EnderDragonEntity.class
-            }));
-        } else {
-            instance.add(priority, goal);
-        }
+    @ModifyArg(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V", ordinal = 9))
+    private Goal dndreams$createDifferentRevengeGoal(Goal instance) {
+        return new RevengeGoal((EndermanEntity) (Object) this, EnderDragonEntity.class);
     }
 }
