1359c38dd04fce234d734f497a8b56b894cad232
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2022-09-23 00:19:32 -0400

    Endermen no longer take revenge against the Ender Dragon.
    Signed-off-by: Eman3600 <98138568+Eman-3600@users.noreply.github.com>

--- src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
+++ src/main/java/net/eman3600/dndreams/mixin/EndermanEntityMixin.java
@@ -3,15 +3,20 @@ package net.eman3600.dndreams.mixin;
 import net.eman3600.dndreams.initializers.ModDimensions;
 import net.eman3600.dndreams.initializers.WorldComponents;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.ai.goal.RevengeGoal;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -33,6 +38,17 @@ public abstract class EndermanEntityMixin extends HostileEntity implements Anger
     private void injectConstructor(EntityType<? extends EndermanEntity> entityType, World world, CallbackInfo info) {
         if (world.getDimensionKey() == ModDimensions.DREAM_TYPE_KEY) {
             this.remove(RemovalReason.DISCARDED);
+        }
+    }
+
+    @Redirect(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V"))
+    private void redirectInitGoals(GoalSelector instance, int priority, Goal goal) {
+        if (goal instanceof RevengeGoal) {
+            instance.add(priority, new RevengeGoal(this, new Class[] {
+                    EnderDragonEntity.class
+            }));
+        } else {
+            instance.add(priority, goal);
         }
     }
 }
