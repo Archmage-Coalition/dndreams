af83393f68b7da58b672541536b0467b3879e623
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2022-10-31 00:11:27 -0400

    Stuff
    Signed-off-by: Eman3600 <98138568+Eman-3600@users.noreply.github.com>

--- src/main/java/net/eman3600/dndreams/blocks/SculkLight.java
+++
@@ -1,4 +0,0 @@
-package net.eman3600.dndreams.blocks;
-
-public class SculkLight {
-}
---
+++ src/main/java/net/eman3600/dndreams/blocks/crop/SculkLeavesBlock.java
@@ -0,0 +1,141 @@
+package net.eman3600.dndreams.blocks.crop;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+public class SculkLeavesBlock
+        extends Block
+        implements Waterloggable {
+    public static final int MAX_DISTANCE = 15;
+    public static final IntProperty DISTANCE = IntProperty.of("distance", 1, 20);
+    public static final BooleanProperty PERSISTENT = Properties.PERSISTENT;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    private static final int field_31112 = 1;
+
+    public SculkLeavesBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(DISTANCE, 20).with(PERSISTENT, false).with(WATERLOGGED, false));
+    }
+
+    @Override
+    public VoxelShape getSidesShape(BlockState state, BlockView world, BlockPos pos) {
+        return VoxelShapes.empty();
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        return state.get(DISTANCE) == 20 && !state.get(PERSISTENT);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (this.shouldDecay(state)) {
+            LeavesBlock.dropStacks(state, world, pos);
+            world.removeBlock(pos, false);
+        }
+    }
+
+    protected boolean shouldDecay(BlockState state) {
+        return !state.get(PERSISTENT) && state.get(DISTANCE) == 20;
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        world.setBlockState(pos, updateDistanceFromLogs(state, world, pos), Block.NOTIFY_ALL);
+    }
+
+    @Override
+    public int getOpacity(BlockState state, BlockView world, BlockPos pos) {
+        return 1;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        int i;
+        if (state.get(WATERLOGGED)) {
+            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        if ((i = getDistanceFromLog(neighborState) + 1) != 1 || state.get(DISTANCE) != i) {
+            world.createAndScheduleBlockTick(pos, this, 1);
+        }
+        return state;
+    }
+
+    private static BlockState updateDistanceFromLogs(BlockState state, WorldAccess world, BlockPos pos) {
+        int i = 20;
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (Direction direction : Direction.values()) {
+            mutable.set(pos, direction);
+            i = Math.min(i, getDistanceFromLog(world.getBlockState(mutable)) + 1);
+            if (i == 1) break;
+        }
+        return state.with(DISTANCE, i);
+    }
+
+    private static int getDistanceFromLog(BlockState state) {
+        if (state.isIn(BlockTags.LOGS)) {
+            return 0;
+        }
+        if (state.getBlock() instanceof SculkLeavesBlock) {
+            return state.get(DISTANCE);
+        }
+        return 20;
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        if (state.get(WATERLOGGED)) {
+            return Fluids.WATER.getStill(false);
+        }
+        return super.getFluidState(state);
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (!world.hasRain(pos.up())) {
+            return;
+        }
+        if (random.nextInt(15) != 1) {
+            return;
+        }
+        BlockPos blockPos = pos.down();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (blockState.isOpaque() && blockState.isSideSolidFullSquare(world, blockPos, Direction.UP)) {
+            return;
+        }
+        double d = (double)pos.getX() + random.nextDouble();
+        double e = (double)pos.getY() - 0.05;
+        double f = (double)pos.getZ() + random.nextDouble();
+        world.addParticle(ParticleTypes.DRIPPING_WATER, d, e, f, 0.0, 0.0, 0.0);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE, PERSISTENT, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        BlockState blockState = this.getDefaultState().with(PERSISTENT, true).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+        return updateDistanceFromLogs(blockState, ctx.getWorld(), ctx.getBlockPos());
+    }
+}
