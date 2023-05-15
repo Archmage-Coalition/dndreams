e46d834921e59cd8ad83a497531293362bd094c1
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2023-05-15 02:58:37 -0400

    Sanity Foods

--- src/main/java/net/eman3600/dndreams/items/TooltipItem.java
+++ src/main/java/net/eman3600/dndreams/items/TooltipItem.java
@@ -1,16 +1,49 @@
 package net.eman3600.dndreams.items;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Objects;
 
 public class TooltipItem extends Item {
-    private boolean defaultTooltip;
+    @Nullable
+    private final String tooltipKey;
+    private final int lines;
 
     public TooltipItem(Settings settings) {
-        super(settings);
+        this(settings, 1);
     }
 
-    public TooltipItem(Settings settings, String tooltipKey) {
-        super(settings);
+    public TooltipItem(Settings settings, @Nullable String tooltipKey) {
+        this(settings, 1, tooltipKey);
+    }
 
+    public TooltipItem(Settings settings, int lines) {
+        this(settings, lines, null);
+    }
+
+    public TooltipItem(Settings settings, int lines, @Nullable String tooltipKey) {
+        super(settings);
+        this.lines = lines;
+        this.tooltipKey = tooltipKey;
+    }
+
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        String key = Objects.requireNonNullElseGet(tooltipKey, () -> getTranslationKey() + ".tooltip");
+
+        if (lines <= 1) {
+            tooltip.add(Text.translatable(key));
+        } else {
+            for (int i = 0; i < lines; i++) {
+                tooltip.add(Text.translatable(key + "." + i));
+            }
+        }
     }
 }
--- src/main/java/net/eman3600/dndreams/util/ModFoodComponents.java
+++ src/main/java/net/eman3600/dndreams/util/ModFoodComponents.java
@@ -5,46 +5,63 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModFoodComponents {
-    public static final FoodComponent NIGHTMARE_FUEL = (new FoodComponent.Builder()).hunger(9).saturationModifier(1.0F)
+
+    public static final Map<FoodComponent, Float> FOODS_TO_SANITY = new HashMap<>();
+
+
+
+
+    public static final FoodComponent NIGHTMARE_FUEL = registerSanityFood(new FoodComponent.Builder().hunger(9).saturationModifier(1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200, 0), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 0), 1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.SUPPRESSED, 130, 0), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent SUCCULENT_APPLE = (new FoodComponent.Builder()).hunger(6).saturationModifier(1.0F)
+            .alwaysEdible().build(), -20);
+    public static final FoodComponent SUCCULENT_APPLE = registerSanityFood(new FoodComponent.Builder().hunger(6).saturationModifier(1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.DREAMY, 400, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.SPIRIT_WARD, 400, 0), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent DRAGONFRUIT = (new FoodComponent.Builder()).hunger(12).saturationModifier(2.5F)
+            .alwaysEdible().build(), 16);
+    public static final FoodComponent DRAGONFRUIT = registerSanityFood(new FoodComponent.Builder().hunger(12).saturationModifier(2.5F)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent POISON_APPLE = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.0F)
+            .alwaysEdible().build(), -4);
+    public static final FoodComponent POISON_APPLE = registerSanityFood(new FoodComponent.Builder().hunger(6).saturationModifier(0.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.SUPPRESSED, 400, 0), 1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.AFFLICTION, 600, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.AETHER, 600, 0), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.WITHER, 200, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent WITHER_BUD = (new FoodComponent.Builder()).hunger(2).saturationModifier(1.0F)
+            .alwaysEdible().build(), -16);
+    public static final FoodComponent WITHER_BUD = registerSanityFood(new FoodComponent.Builder().hunger(2).saturationModifier(1.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.AFFLICTION, 600, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.WITHER, 300, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent RAVAGED_FLESH = (new FoodComponent.Builder()).hunger(4).saturationModifier(0.0F)
+            .alwaysEdible().build(), -16);
+    public static final FoodComponent RAVAGED_FLESH = registerSanityFood(new FoodComponent.Builder().hunger(4).saturationModifier(0.0F)
             .statusEffect(new StatusEffectInstance(ModStatusEffects.LOOMING, 3600, 3), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 600, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200, 1), 1.0F)
-            .alwaysEdible().build();
-    public static final FoodComponent LOTUS_FLOWER = (new FoodComponent.Builder()).hunger(5).saturationModifier(0.8F)
-            .build();
-    public static final FoodComponent STAR_FRUIT = (new FoodComponent.Builder()).hunger(6).saturationModifier(1.0F)
-            .build();
+            .alwaysEdible().build(), -16);
+    public static final FoodComponent LOTUS_FLOWER = registerSanityFood(new FoodComponent.Builder().hunger(5).saturationModifier(0.8F)
+            .build(), 3);
+    public static final FoodComponent STAR_FRUIT = registerSanityFood(new FoodComponent.Builder().hunger(6).saturationModifier(1.0F)
+            .build(), 8);
+
+
+
     public static FoodComponent cakewood(float planks) {
-        return (new FoodComponent.Builder()).hunger((int)(3 * planks)).saturationModifier(0.6f)
+        return registerSanityFood(new FoodComponent.Builder().hunger((int)(3 * planks)).saturationModifier(0.6f)
                 .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, (int)(80 * planks)), 1.0f)
                 .alwaysEdible()
-                .build();
+                .build(), planks);
+    }
+
+    public static FoodComponent registerSanityFood(FoodComponent component, float sanity) {
+        FOODS_TO_SANITY.put(component, sanity);
+        return component;
     }
 }
