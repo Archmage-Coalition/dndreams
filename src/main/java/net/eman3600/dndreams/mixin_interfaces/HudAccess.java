720f6460b1e433f8f12c8945613b1bd4f213ff18
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2023-06-15 13:38:00 -0400

    REjuvenation Hearts

--- src/main/java/net/eman3600/dndreams/mixin_interfaces/HudAccess.java
+++ src/main/java/net/eman3600/dndreams/mixin_interfaces/HudAccess.java
@@ -10,21 +10,30 @@ public interface HudAccess {
     enum CustomHeartType {
 
         NO_CHANGE(0),
-        AFFLICTION(1);
+        AFFLICTION(1),
+        MORTAL(2),
+        MORTAL_CONTAINER(2, true),
+        REJUVENATION(3);
 
         private final int id;
+        private final boolean container;
+
+        CustomHeartType(int id, boolean container) {
+            this.id = id;
+            this.container = container;
+        }
 
         CustomHeartType(int id) {
-            this.id = id;
+            this(id, false);
         }
 
         public static CustomHeartType fromPlayerState(PlayerEntity player, InGameHud.HeartType type) {
             if (player == null) return NO_CHANGE;
 
-            if (type == InGameHud.HeartType.CONTAINER) return NO_CHANGE;
+            if (type == InGameHud.HeartType.CONTAINER) return player.hasStatusEffect(ModStatusEffects.MORTAL) ? MORTAL_CONTAINER : NO_CHANGE;
             if (type == InGameHud.HeartType.ABSORBING) return NO_CHANGE;
 
-            return player.hasStatusEffect(ModStatusEffects.AFFLICTION) ? AFFLICTION : NO_CHANGE;
+            return player.hasStatusEffect(ModStatusEffects.MORTAL) ? MORTAL : player.hasStatusEffect(ModStatusEffects.AFFLICTION) ? AFFLICTION : player.hasStatusEffect(ModStatusEffects.REJUVENATION) ? REJUVENATION : NO_CHANGE;
         }
 
         public int getU() {
@@ -32,13 +41,18 @@ public interface HudAccess {
         }
 
         public int getV(boolean half, boolean blinking, boolean hardcore) {
-            int v = 0;
+            if (container) {
+                return blinking ? 81 : 72;
+            }
+            else {
+                int v = 0;
 
-            if (half) v += 9;
-            if (blinking) v += 18;
-            if (hardcore) v += 36;
+                if (half) v += 9;
+                if (blinking) v += 18;
+                if (hardcore) v += 36;
 
-            return v;
+                return v;
+            }
         }
     }
 }
