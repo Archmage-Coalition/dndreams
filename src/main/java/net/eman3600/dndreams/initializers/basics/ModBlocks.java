ef053a9bb74470ecabff0b0d4f599402b3e523ae
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2024-01-06 02:53:19 -0500

    They said they didn't exist...

--- src/main/java/net/eman3600/dndreams/initializers/basics/ModBlocks.java
+++ src/main/java/net/eman3600/dndreams/initializers/basics/ModBlocks.java
@@ -576,60 +576,60 @@ public class ModBlocks {
 
 
     // Nightshade Wood
-    public static final Block SHADE_LOG = registerBlock("shade_log",
+    public static final Block SHADE_LOG = registerDreamBlock("shade_log",
             new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block SHADE_WOOD = registerBlock("shade_wood",
+    public static final Block SHADE_WOOD = registerDreamBlock("shade_wood",
             new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block STRIPPED_SHADE_LOG = registerBlock("shade_stripped_log",
+    public static final Block STRIPPED_SHADE_LOG = registerDreamBlock("shade_stripped_log",
             new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block STRIPPED_SHADE_WOOD = registerBlock("shade_stripped_wood",
+    public static final Block STRIPPED_SHADE_WOOD = registerDreamBlock("shade_stripped_wood",
             new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
 
-    public static final Block SHADE_PLANKS = registerBlock("shade_planks",
+    public static final Block SHADE_PLANKS = registerDreamBlock("shade_planks",
             new Block(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
 
-    public static final Block SHADE_LEAVES = registerBlock("shade_leaves",
+    public static final Block SHADE_LEAVES = registerDreamBlock("shade_leaves",
             new LeavesBlock(FabricBlockSettings.copy(Blocks.AZALEA_LEAVES)
                     .nonOpaque().ticksRandomly().suffocates((state, world, pos) -> false).blockVision((state, world, pos) -> false)),
 
             new FabricItemSettings().group(ItemGroup.DECORATIONS));
-    public static final Block SHADE_BUSH = registerBlock("shade_bush",
+    public static final Block SHADE_BUSH = registerDreamBlock("shade_bush",
             new ShadeSaplingBlock(FabricBlockSettings.copy(Blocks.AZALEA)
                     .breakInstantly().suffocates((state, world, pos) -> false).blockVision((state, world, pos) -> false)),
 
             new FabricItemSettings().group(ItemGroup.DECORATIONS));
 
-    public static final Block SHADE_STAIRS = registerBlock("shade_stairs",
+    public static final Block SHADE_STAIRS = registerDreamBlock("shade_stairs",
             new ModStairsBlock(SHADE_PLANKS.getDefaultState(), FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block SHADE_SLAB = registerBlock("shade_slab",
+    public static final Block SHADE_SLAB = registerDreamBlock("shade_slab",
             new SlabBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block SHADE_FENCE = registerBlock("shade_fence",
+    public static final Block SHADE_FENCE = registerDreamBlock("shade_fence",
             new FenceBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
             new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block SHADE_FENCE_GATE = registerBlock("shade_fence_gate",
+    public static final Block SHADE_FENCE_GATE = registerDreamBlock("shade_fence_gate",
             new FenceGateBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f)
                     .sounds(BlockSoundGroup.WOOD)),
 
@@ -1032,17 +1032,17 @@ public class ModBlocks {
 
             new FabricItemSettings().group(ItemGroup.DECORATIONS));
 
-    public static final Block SHADE_WEED = registerBlock("shade_weed",
+    public static final Block SHADE_WEED = registerDreamBlock("shade_weed",
             new ShadeWeedBlock(FabricBlockSettings.copy(Blocks.GRASS)
                     .nonOpaque()),
 
             new FabricItemSettings().group(ItemGroup.DECORATIONS));
-    public static final Block SHADE_GRASS = registerBlock("shade_grass",
+    public static final Block SHADE_GRASS = registerDreamBlock("shade_grass",
             new ShadePlantBlock(FabricBlockSettings.copy(Blocks.GRASS)
                     .nonOpaque()),
 
             new FabricItemSettings().group(ItemGroup.DECORATIONS));
-    public static final Block SHADE_FERN = registerBlock("shade_fern",
+    public static final Block SHADE_FERN = registerDreamBlock("shade_fern",
             new ShadePlantBlock(FabricBlockSettings.copy(Blocks.FERN)
                     .nonOpaque()),
 
--- src/main/java/net/eman3600/dndreams/initializers/event/ModMessages.java
+++ src/main/java/net/eman3600/dndreams/initializers/event/ModMessages.java
@@ -1,6 +1,5 @@
 package net.eman3600.dndreams.initializers.event;
 
-import net.eman3600.dndreams.Initializer;
 import net.eman3600.dndreams.networking.packet_c2s.AirSwingPacket;
 import net.eman3600.dndreams.networking.packet_c2s.DodgePacket;
 import net.eman3600.dndreams.networking.packet_s2c.*;
@@ -8,17 +7,19 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.util.Identifier;
 
+import static net.eman3600.dndreams.Initializer.MODID;
+
 public class ModMessages {
 
-    public static final Identifier AIR_SWING_ID = new Identifier(Initializer.MODID, "air_swing");
-    public static final Identifier DODGE_ID = new Identifier(Initializer.MODID, "dodge");
+    public static final Identifier AIR_SWING_ID = new Identifier(MODID, "air_swing");
+    public static final Identifier DODGE_ID = new Identifier(MODID, "dodge");
 
-    public static final Identifier DRAGON_FLASH_ID = new Identifier(Initializer.MODID, "dragon_flash");
-    public static final Identifier CROWNED_SLASH_ID = new Identifier(Initializer.MODID, "crowned_slash");
-    public static final Identifier CROWNED_BEAM_ID = new Identifier(Initializer.MODID, "crowned_beam");
-    public static final Identifier TESLA_SLASH_ID = new Identifier(Initializer.MODID, "tesla_slash");
-    public static final Identifier ANCIENT_PORTAL_SOUND_ID = new Identifier(Initializer.MODID, "ancient_portal_sound");
-    public static final Identifier ENERGY_PARTICLE_ID = new Identifier(Initializer.MODID, "energy_particle");
+    public static final Identifier DRAGON_FLASH_ID = new Identifier(MODID, "dragon_flash");
+    public static final Identifier CROWNED_SLASH_ID = new Identifier(MODID, "crowned_slash");
+    public static final Identifier CROWNED_BEAM_ID = new Identifier(MODID, "crowned_beam");
+    public static final Identifier TESLA_SLASH_ID = new Identifier(MODID, "tesla_slash");
+    public static final Identifier ANCIENT_PORTAL_SOUND_ID = new Identifier(MODID, "ancient_portal_sound");
+    public static final Identifier ENERGY_PARTICLE_ID = new Identifier(MODID, "energy_particle");
 
     public static void registerC2SPackets() {
         ServerPlayNetworking.registerGlobalReceiver(AIR_SWING_ID, AirSwingPacket::receive);
