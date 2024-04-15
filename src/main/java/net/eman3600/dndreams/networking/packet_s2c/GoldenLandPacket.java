d90abc42586fbdb60cb21f8cca7b8da38f24f4a7
Eman3600 <98138568+Eman-3600@users.noreply.github.com>
2024-04-15 02:17:22 -0400

    Updated Golden Land

--- src/main/java/net/eman3600/dndreams/networking/packet_s2c/GoldenLandPacket.java
+++ src/main/java/net/eman3600/dndreams/networking/packet_s2c/GoldenLandPacket.java
@@ -10,7 +10,6 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -19,20 +18,31 @@ import net.minecraft.util.math.Vec3d;
 
 public class GoldenLandPacket {
 
+    private static final int DISTANCE = 2;
+    private static final float HALF_DIST = DISTANCE * .5f;
+    private static final float SPACING = .5f;
+
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf packet, PacketSender sender) {
         ClientWorld world = client.world;
 
         Vec3d pos = new Vec3d(packet.readDouble(), packet.readDouble(), packet.readDouble());
         float yaw = packet.readFloat();
 
-        Vec3d offset = AirSwingItem.rayXVector(yaw, 0).multiply(.5f);
+        Vec3d offsetX = AirSwingItem.rayXVector(yaw, 0).multiply(SPACING);
+        Vec3d offsetZ = AirSwingItem.rayZVector(yaw, 0).multiply(SPACING);
 
-        pos = pos.add(offset.multiply(-3));
+        renderLine(world, pos.add(offsetZ.multiply(-HALF_DIST)).add(offsetX.multiply(-DISTANCE)), offsetX);
+        renderLine(world, pos.add(offsetZ.multiply(HALF_DIST)).add(offsetX.multiply(-DISTANCE)), offsetX);
+        renderLine(world, pos.add(offsetX.multiply(-HALF_DIST)).add(offsetZ.multiply(-DISTANCE)), offsetZ);
+        renderLine(world, pos.add(offsetX.multiply(HALF_DIST)).add(offsetZ.multiply(-DISTANCE)), offsetZ);
+    }
 
-        for (int i = -3; i <= 3; i++) {
+    private static void renderLine(ClientWorld world, Vec3d pos, Vec3d dir) {
+
+        for (int i = -DISTANCE; i <= DISTANCE; i++) {
             world.addParticle(ModParticles.GOLDEN_LAND, pos.x, pos.y, pos.z, 0, 0, 0);
 
-            pos = pos.add(offset);
+            pos = pos.add(dir);
         }
     }
 
