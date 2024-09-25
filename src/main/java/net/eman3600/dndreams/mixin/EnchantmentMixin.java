1611a4f5647269435c49632b2af6b6fb8b565264
Eman3600 <real.eman3600@gmail.com>
2024-09-25 15:58:19 -0400

    Buffed instant madness/silence; Cloud tools cannot be enchanted with fortune or silk touch

--- src/main/java/net/eman3600/dndreams/mixin/EnchantmentMixin.java
+++ src/main/java/net/eman3600/dndreams/mixin/EnchantmentMixin.java
@@ -2,10 +2,7 @@ package net.eman3600.dndreams.mixin;
 
 import net.eman3600.dndreams.items.interfaces.ManaCostItem;
 import net.eman3600.dndreams.util.ModTags;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.InfinityEnchantment;
-import net.minecraft.enchantment.MendingEnchantment;
-import net.minecraft.enchantment.UnbreakingEnchantment;
+import net.minecraft.enchantment.*;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +14,7 @@ public abstract class EnchantmentMixin {
 
     @Inject(method = "isAcceptableItem", at = @At("RETURN"), cancellable = true)
     private void dndreams$isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if ((((Object)this) instanceof InfinityEnchantment && stack.getItem() instanceof ManaCostItem) || (((Object)this instanceof UnbreakingEnchantment || (Object)this instanceof MendingEnchantment) && stack.isIn(ModTags.AUTO_REPAIRING_TOOLS))) {
+        if ((((Object)this) instanceof InfinityEnchantment && stack.getItem() instanceof ManaCostItem) || (((Object)this instanceof UnbreakingEnchantment || (Object)this instanceof MendingEnchantment) && stack.isIn(ModTags.AUTO_REPAIRING_TOOLS)) || (((Object)this instanceof SilkTouchEnchantment || (Object)this == Enchantments.FORTUNE) && stack.isIn(ModTags.SILKY_TOOLS))) {
             cir.setReturnValue(false);
         }
     }
